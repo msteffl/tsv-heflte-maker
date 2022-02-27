8ca45e5d5fdc782268bfe184405c0ef4a1a4e44c
smf2abt <michael.steffl2@bosch.com>
2022-02-27 19:13:58 +0100

    chore: uncomment

--- src/index.ts
+++ src/index.ts
@@ -15,53 +15,53 @@ const csvOptions = {
   quote: ''
 }
 
-// const fupaStatsErste: FupaStatistics = new FupaStatistics('erste')
-// fupaStatsErste.create().then(data => {
-//   const parser = new Parser(csvOptions)
-//   const csv = parser.parse(data)
-//   const path = OUTPUT_PATH + '/ErsteStatistik.txt'
-//   Fs.writeFileSync(path, csv)
-// })
+const fupaStatsErste: FupaStatistics = new FupaStatistics('erste')
+fupaStatsErste.create().then(data => {
+  const parser = new Parser(csvOptions)
+  const csv = parser.parse(data)
+  const path = OUTPUT_PATH + '/ErsteStatistik.txt'
+  Fs.writeFileSync(path, csv)
+})
 
-// const fupaStatsZweite: FupaStatistics = new FupaStatistics('zweite')
-// fupaStatsZweite.create().then(data => {
-//   const parser = new Parser(csvOptions)
-//   const csv = parser.parse(data)
-//   const path = OUTPUT_PATH + '/ZweiteStatistik.txt'
-//   Fs.writeFileSync(path, csv)
-// })
+const fupaStatsZweite: FupaStatistics = new FupaStatistics('zweite')
+fupaStatsZweite.create().then(data => {
+  const parser = new Parser(csvOptions)
+  const csv = parser.parse(data)
+  const path = OUTPUT_PATH + '/ZweiteStatistik.txt'
+  Fs.writeFileSync(path, csv)
+})
 
-// const fupaTableErste = new FupaTable('erste')
-// fupaTableErste.create().then(data => {
-//   const parser = new Parser(csvOptions)
-//   const csv = parser.parse(data)
-//   const path = OUTPUT_PATH + '/ErsteTabelle.txt'
-//   Fs.writeFileSync(path, csv)
-// })
+const fupaTableErste = new FupaTable('erste')
+fupaTableErste.create().then(data => {
+  const parser = new Parser(csvOptions)
+  const csv = parser.parse(data)
+  const path = OUTPUT_PATH + '/ErsteTabelle.txt'
+  Fs.writeFileSync(path, csv)
+})
 
-// const fupaTableZweite = new FupaTable('zweite')
-// fupaTableZweite.create().then(data => {
-//   const parser = new Parser(csvOptions)
-//   const csv = parser.parse(data)
-//   const path = OUTPUT_PATH + '/ZweiteTabelle.txt'
-//   Fs.writeFileSync(path, csv)
-// })
+const fupaTableZweite = new FupaTable('zweite')
+fupaTableZweite.create().then(data => {
+  const parser = new Parser(csvOptions)
+  const csv = parser.parse(data)
+  const path = OUTPUT_PATH + '/ZweiteTabelle.txt'
+  Fs.writeFileSync(path, csv)
+})
 
-// const fussballdeMatchesErste: FussballdeMatches = new FussballdeMatches('erste', 16, "15:00")
-// fussballdeMatchesErste.create().then(data => {
-//   const parser = new Parser(csvOptions)
-//   const csv = parser.parse(data)
-//   const path = OUTPUT_PATH + '/ErsteBegegnungen.txt'
-//   Fs.writeFileSync(path, csv)
-// })
+const fussballdeMatchesErste: FussballdeMatches = new FussballdeMatches('erste', 16, "15:00")
+fussballdeMatchesErste.create().then(data => {
+  const parser = new Parser(csvOptions)
+  const csv = parser.parse(data)
+  const path = OUTPUT_PATH + '/ErsteBegegnungen.txt'
+  Fs.writeFileSync(path, csv)
+})
 
-// const fussballdeMatchesZweite: FussballdeMatches = new FussballdeMatches('zweite', 16, "13:00")
-// fussballdeMatchesZweite.create().then(data => {
-//   const parser = new Parser(csvOptions)
-//   const csv = parser.parse(data)
-//   const path = OUTPUT_PATH + '/ZweiteBegegnungen.txt'
-//   Fs.writeFileSync(path, csv)
-// })
+const fussballdeMatchesZweite: FussballdeMatches = new FussballdeMatches('zweite', 16, "13:00")
+fussballdeMatchesZweite.create().then(data => {
+  const parser = new Parser(csvOptions)
+  const csv = parser.parse(data)
+  const path = OUTPUT_PATH + '/ZweiteBegegnungen.txt'
+  Fs.writeFileSync(path, csv)
+})
 
 const fupaSpielplan: FupaSpielplan = new FupaSpielplan()
 fupaSpielplan.createComplete().then(data => {
